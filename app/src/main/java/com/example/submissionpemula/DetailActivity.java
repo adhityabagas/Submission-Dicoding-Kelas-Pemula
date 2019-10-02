25240e42fac70bb4ab8b042b5033be28dee1a159
Adhitya Bagasmiwa Permana <adhityab2000@gmail.com>
2019-10-02 10:38:06 +0700

    Added Class Java, Layout XML, and Modified Android Manifest

--- app/src/main/java/com/example/submissionpemula/DetailActivity.java
+++ app/src/main/java/com/example/submissionpemula/DetailActivity.java
@@ -1,4 +1,43 @@
 package com.example.submissionpemula;
 
-public class DetailActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+public class DetailActivity extends AppCompatActivity {
+    public static final String EXTRA_CAMERA = "EXTRA_CAMERA";
+    ImageView imgDetailPhoto;
+    TextView tvTextName, tvTextType, tvTextPrice, tvTextDesc;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        imgDetailPhoto = findViewById(R.id.img_icon_detail);
+        tvTextName = findViewById(R.id.tv_item_name_detail);
+        tvTextType = findViewById(R.id.tv_item_jenis_detail);
+        tvTextPrice = findViewById(R.id.tv_item_price_detail);
+        tvTextDesc = findViewById(R.id.tv_desc_detail);
+        Camera camera = getIntent().getParcelableExtra(EXTRA_CAMERA);
+
+        Glide.with(this)
+                .load(camera.getPhoto())
+                .apply(new RequestOptions())
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                .into(imgDetailPhoto);
+
+        tvTextName.setText(camera.getName());
+        tvTextType.setText(camera.getType());
+        tvTextPrice.setText(camera.getPrice());
+        tvTextDesc.setText(camera.getDesc());
+    }
 }
--- app/src/main/java/com/example/submissionpemula/MainActivity.java
+++ app/src/main/java/com/example/submissionpemula/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.submissionpemula;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rvCamera;
+    private ArrayList<Camera> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvCamera = findViewById(R.id.rv_camera);
+        rvCamera.setHasFixedSize(true);
+
+        list.addAll(CamerasData.getLisData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvCamera.setLayoutManager(new LinearLayoutManager(this));
+        ListCameraAdapter listCameraAdapter = new ListCameraAdapter(this);
+        listCameraAdapter.setListCameraAdapter(list);
+        rvCamera.setAdapter(listCameraAdapter);
+        listCameraAdapter.setOnItemClickCallback(new ListCameraAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(Camera data) {
+
+            }
+        });
     }
 }
